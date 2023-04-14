49899a647038c47d2a490a917faf24afa9f5bec3
jhonned01 <jhonned01@gmail.com>
2023-04-14 17:57:49 -0500

    videos

--- app/InfoNinos/page.tsx
+++ app/InfoNinos/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 import TitulosInternos from "../Inicio/TitulosInternos";
 import InfoNinosItem from "./InfoNinosItem";
 import { useEffect, useState } from "react";
+import ReactPlayer from "react-player/youtube";
 
-function Index({ infoNino }: any) {
+function InfoNinos() {
   const [Data, setData] = useState({} as any);
 
   const GetData = async () => {
@@ -18,65 +19,60 @@ function Index({ infoNino }: any) {
 
   console.log("Data", Data);
 
-  //   const opts = {
-  //     height: "390",
-  //     width: "100%",
-  //   };
-
-  const showInfoNinos = () => (
-    <>
-      {Data?.infoNino?.length > 0 ? (
-        <div className="flex flex-wrap p-3">
-          {Data?.infoNino?.map((item: any) => {
-            return (
-              <div
-                key={item.id}
-                className="flex flex-wrap pr-[1.1rem] justify-center"
-              >
-                <InfoNinosItem
-                  key={item.id}
-                  title={item.tema}
-                  subtitle1="Grado:"
-                  contenido={item.grado}
-                  subtitle2="Area:"
-                  contenido2={item.area}
-                  subtitle3="Docente:"
-                  contenido3={item.docente}
-                  subtitle4="Fecha:"
-                  fecha={item.fecha}
-                />
-                {/* <YouTube
-                  className="border-b-2 mb-4 lg:border-none lg:mb-0 pb-6 col-span-2"
-                  videoId={`${item.archivo}`}
-                  opts={opts}
-                /> */}
-              </div>
-            );
-          })}
-        </div>
-      ) : (
-        <div className="container lg:w-[700px] flex flex-col p-4 mx-auto col-span-3">
-          <Image
-            className="object-cover"
-            src="/Menu/PendienteWebMaster.png"
-            width={1400}
-            height={940}
-            alt="PendienteWebMaster"
-          />
-          <p className="error">
-            Registro pendiente por publicar desde WebMaster
-          </p>
-        </div>
-      )}
-    </>
-  );
-
   return (
     <div>
       <TitulosInternos title="Información para niños, niñas y adolescentes" />
-      {showInfoNinos()}
+      {Object.keys(Data).length > 0 && (
+        <>
+          {Data?.infoNino?.length > 0 ? (
+            <div className="flex flex-wrap p-3">
+              {Data?.infoNino?.map((item: any) => {
+                return (
+                  <div
+                    key={item.id}
+                    className="flex flex-wrap pr-[1.1rem] justify-center"
+                  >
+                    <InfoNinosItem
+                      key={item.id}
+                      title={item.tema}
+                      subtitle1="Grado:"
+                      contenido={item.grado}
+                      subtitle2="Area:"
+                      contenido2={item.area}
+                      subtitle3="Docente:"
+                      contenido3={item.docente}
+                      subtitle4="Fecha:"
+                      fecha={item.fecha}
+                    />
+                    <ReactPlayer
+                      className="border-b-2 mb-4 lg:border-none lg:mb-0 pb-6 col-span-2 "
+                      url={`${item.archivo}`}
+                      width="400px"
+                      height={"200px"}
+                      controls={true}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          ) : (
+            <div className="container lg:w-[700px] flex flex-col p-4 mx-auto col-span-3">
+              <Image
+                className="object-cover"
+                src="/Menu/PendienteWebMaster.png"
+                width={1400}
+                height={940}
+                alt="PendienteWebMaster"
+              />
+              <p className="error">
+                Registro pendiente por publicar desde WebMaster
+              </p>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
 
-export default Index;
+export default InfoNinos;
