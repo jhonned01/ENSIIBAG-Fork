53f8e41879cafc6e2ca37f73878960cc4746313d
jhonned01 <jhonned01@gmail.com>
2023-04-17 17:42:00 -0500

    bg

--- app/Inicio/Slider.tsx
+++ app/Inicio/Slider.tsx
@@ -11,7 +11,7 @@ const Slider = ({ imageSlider }: any) => {
       showThumbs={false}
       showStatus={false}
       interval={5000}
-      className="w-full h-[500px]  bg-red-900 "
+      className="w-full h-[500px]  "
     >
       {imageSlider.map((slider: any) => (
         <div key={slider?.lugar}>
