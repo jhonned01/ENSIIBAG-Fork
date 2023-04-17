9abc59f4ca44c8ad9c6eebc8acbb3ae069751b76
MiguelOlarte06 <miguelolarte.2606@gmail.com>
2023-04-17 18:03:54 -0500

    ya casi esta esta la normal en js

--- app/Galerias/page.tsx
+++ app/Galerias/page.tsx
@@ -16,47 +16,42 @@ function Index() {
     GetData();
   }, []);
 
-  const ShowData = () => {
-    // if (Data?.galeria[0]?.msn || Data?.galeria[1]?.error) {
-    //   return (
-    //     <div>
-    //       <p className="error text-center">{galeria[0]?.msn}</p>
-    //       <p className="error text-center">{galeria[1]?.error}</p>
-    //     </div>
-    //   );
-    // }
-
-    if (Data?.galeria && Data?.galeria.length >= 1) {
-      return Data?.galeria.map((item: any) => (
-        <GaleriaItem
-          key={item.id}
-          title={item.descripcion}
-          imgGalerias={`https://ensiibague.edu.co/portal/img/${item.imagen}`}
-        />
-      ));
-    } else {
-      return (
-        <div className="container lg:w-[700px] flex flex-col p-4 mx-auto col-span-3">
-          <Image
-            className="object-cover"
-            src="/Menu/PendienteWebMaster.png"
-            width={1400}
-            height={940}
-            alt="PendienteWebMaster"
-          />
-          <p className="error">
-            Registro pendiente por publicar desde WebMaster
-          </p>
-        </div>
-      );
-    }
-  };
-
   return (
     <div>
       <TitulosInternos title="Galerías" />
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 ">
-        {ShowData()}
+        {Object.keys(Data)?.length > 0 && (
+          <>
+            {Data?.galeria?.length >= 1 ? (
+              <>
+                {Data?.galeria?.map((item: any) => (
+                  <GaleriaItem
+                    key={item.id}
+                    title={item.descripcion}
+                    imgGalerias={`https://ensiibague.edu.co/portal/img/${item.imagen}`}
+                  />
+                ))}
+              </>
+            ) : (
+              <>
+                {Data?.galeria?.length == 0 && (
+                  <div className="container lg:w-[700px] flex flex-col p-4 mx-auto col-span-3">
+                    <Image
+                      className="object-cover"
+                      src="/Menu/PendienteWebMaster.png"
+                      width={1400}
+                      height={940}
+                      alt="PendienteWebMaster"
+                    />
+                    <p className="error">
+                      Registro pendiente por publicar desde WebMaster
+                    </p>
+                  </div>
+                )}
+              </>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
