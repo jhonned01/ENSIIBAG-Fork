eb324e6d46bae6f853a121a3eec42ff8e7f2dfbc
jhonned01 <jhonned01@gmail.com>
2023-05-05 17:35:07 -0500

    change item dynamic

--- app/ItemSubmenuDynamic/[id]/page.tsx
+++ app/ItemSubmenuDynamic/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import CompCargando from "@/app/Inicio/CompCargando";
 import TitulosInternos from "@/app/Inicio/TitulosInternos";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
@@ -17,7 +18,10 @@ const ItemSubmenuDynamic = ({ params: { id } }: any) => {
         const info = await fetch(`/api/ItemSubmenuDynamic/${id}`).then((res) =>
           res.json()
         );
-        setData(info?.ItemSubMenu);
+
+        console.log(info);
+
+        setData(info?.ItemSubMenu || {});
         setLoading(false);
       } catch (error) {
         console.log(error);
@@ -36,11 +40,30 @@ const ItemSubmenuDynamic = ({ params: { id } }: any) => {
     <div>
       {!Loading && Object.keys(Data)?.length > 0 ? (
         <>
-          <TitulosInternos title={Data?.name} />
-          <div
-            className="container mx-auto flex flex-col items-center justify-center p-4"
-            dangerouslySetInnerHTML={{ __html: `${Data?.contenido || ""}` }}
-          />
+          <TitulosInternos title={Data?.name || "Ruta Incorrecta"} />
+
+          {Data?.contenido?.length ? (
+            <>
+              <div
+                className="container mx-auto flex flex-col items-center justify-center p-4"
+                dangerouslySetInnerHTML={{ __html: `${Data?.contenido || ""}` }}
+              />
+            </>
+          ) : (
+            <>
+              <div className="flex flex-col justify-center items-center">
+                <h1 className="text-gray-800 text-3xl font-semibold animate-pulse pt-2">
+                  Registro pendiente por subir por parte de la institución. .
+                </h1>
+                <Image
+                  src={"/Menu/PendienteWebMaster.png"}
+                  width={500}
+                  height={500}
+                  alt="Pendiente por subir WebMaster"
+                />
+              </div>
+            </>
+          )}
         </>
       ) : (
         <> {Loading && <CompCargando />}</>
