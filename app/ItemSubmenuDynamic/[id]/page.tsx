ce76a96f413e7d81ade578c88f851d6244751eef
jhonned01 <jhonned01@gmail.com>
2023-05-05 16:55:00 -0500

    mejorando las dinamic rouutes

--- app/ItemSubmenuDynamic/[id]/page.tsx
+++ app/ItemSubmenuDynamic/[id]/page.tsx
@@ -1,16 +1,31 @@
 "use client";
+import CompCargando from "@/app/Inicio/CompCargando";
 import TitulosInternos from "@/app/Inicio/TitulosInternos";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const ItemSubmenuDynamic = ({ params: { id } }: any) => {
   const [Data, setData] = useState({} as any);
+  const [Loading, setLoading] = useState(false);
+
+  const router = useRouter();
 
   useEffect(() => {
     const GetData = async () => {
-      const infos = await fetch(`/api/ItemSubmenuDynamic/${id}`).then((res) =>
-        res.json()
-      );
-      setData(infos?.ItemSubMenu);
+      try {
+        setLoading(true);
+        const infos = await fetch(`/api/ItemSubmenuDynamic/${id}`).then((res) =>
+          res.json()
+        );
+        setData(infos?.ItemSubMenu);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+
+        alert("Error al cargar la información");
+        setLoading(false);
+        router.replace("/");
+      }
     };
     if (id) {
       GetData();
@@ -19,7 +34,7 @@ const ItemSubmenuDynamic = ({ params: { id } }: any) => {
 
   return (
     <div>
-      {Object.keys(Data)?.length > 0 && (
+      {!Loading && Object.keys(Data)?.length > 0 ? (
         <>
           <TitulosInternos title={Data?.name} />
           <div
@@ -27,6 +42,8 @@ const ItemSubmenuDynamic = ({ params: { id } }: any) => {
             dangerouslySetInnerHTML={{ __html: `${Data?.contenido || ""}` }}
           />
         </>
+      ) : (
+        <> {Loading && <CompCargando />}</>
       )}
     </div>
   );
